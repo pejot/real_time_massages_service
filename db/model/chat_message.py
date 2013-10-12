37613b308b343121e297e6b7ef9bbef3b5a2d9aa
pejot <pejot@pejot-linux-station.(none)>
2013-10-12 15:52:05 +0200

    delvered and read fields added to Message model. sender_id abstracted to Message model.

--- db/model/chat_message.py
+++ db/model/chat_message.py
@@ -1,7 +1,7 @@
 from sqlalchemy import Column, Integer
 from db import autoupdate
 from db.model.message import Message
-from sqlalchemy import ForeignKey, CheckConstraint
+from sqlalchemy import ForeignKey
 
 
 class ChatMessage(Message):
@@ -13,9 +13,7 @@ class ChatMessage(Message):
     __tablename__ = 'chat_messages'
 
     id = Column(Integer, ForeignKey('messages.id'), primary_key=True)
-    sender_id = Column(Integer, ForeignKey('users.id'))
-    receiver_id = Column(Integer, ForeignKey('users.id'), CheckConstraint(
-        'sender_id != receiver_id', name='check_sender_receiver_uniqueness'))
+    receiver_id = Column(Integer, ForeignKey('users.id'))
     
     def __init__(self, content, sender, receiver):
         if content is None:
@@ -26,6 +24,9 @@ class ChatMessage(Message):
             raise ValueError("Given sender is None")
         if receiver is None:
             raise ValueError("Given receiver is None")
+        #can be migrated to model constraint field
+        if receiver == sender:
+            raise ValueError("Receiver and sender must be different")
         self.content = content
         self.sender_id = sender.id
         self.receiver_id = receiver.id
--- db/model/message.py
+++ db/model/message.py
@@ -1,7 +1,8 @@
 from db.backend import Backend
-from sqlalchemy import Column, Integer, String, DateTime
+from sqlalchemy import Column, Integer, String, DateTime, Boolean
 from db import autoupdate
 import datetime
+from sqlalchemy import ForeignKey
 
 class Message(Backend.instance().get_base()):
 
@@ -12,8 +13,11 @@ class Message(Backend.instance().get_base()):
     __tablename__ = 'messages'
 
     id = Column(Integer, primary_key=True, autoincrement=True)
+    sender_id = Column(Integer, ForeignKey('users.id')) 
     content = Column(String, nullable=False)
     created_date = Column(DateTime, nullable=False, default=datetime.datetime.utcnow)
+    delivered = Column(Boolean, default = False)
+    read = Column(Boolean, default = False)
 
     def __init__(self):
         #make class abstract
